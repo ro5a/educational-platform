f7aea474f4f8432d5cc2676aca75195b17f2867e
roaa-hakimi <108170193+roaa-hakimi@users.noreply.github.com>
2024-09-03 12:29:20 +0300

    create roles - permissions

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,9 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('roles-permissions')" :active="request()->routeIs('roles-permissions')">
+                        {{ __('roles-permissions') }}
+                    </x-nav-link>
                 </div>
             </div>
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/dashboard', [USerController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/roles-permissions', function () {
+    return view('vendor/laratrust/panel/roles/index');
+})->name('roles-permissions');
 
 
 Route::controller(USerController::class)->group(function () {
